31ff69e61cc33366a66d48928f08b8e1f20bc5bc
anilkothattil <36529003+anilkothattil@users.noreply.github.com>
2018-03-20 14:40:08 +0530

    Update server.java

--- server.java
+++ server.java
@@ -1,4 +1,11 @@
 import java.net.*;            
 import java.io.InputStream;
 import java.io.DataInputStream;
+public class WishesServer
+{
+   public static void main(String args[]) throws Exception
+   {
+     ServerSocket sersock = new ServerSocket(5000); 
+     System.out.println("server is ready");  //  message to know the server is running
  
+     Socket sock = sersock.accept();               
