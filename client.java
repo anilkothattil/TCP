cd603a34d7758061c44143e7033c42b4ed788cac
anilagraceprakash <anilagracep@gmail.com>
2018-03-20 14:48:03 +0530

    added

--- client.java
+++ client.java
@@ -2,3 +2,12 @@ import java.net.*;
 import java.io.OutputStream;
 import java.io.DataOutputStream;
 
+public class WishesClient
+{
+   public static void main(String args[]) throws Exception
+   {
+     Socket sock = new Socket("localhost", 5000);
+	InetAddress ip=sock.getInetAddress();
+	System.out.println(ip);       
+     String message1 = "Accept Best Wishes, Server";
+ 
