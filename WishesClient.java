946f226cb453ded20989ca43369b79b883bf223d
anilagraceprakash <anilagracep@gmail.com>
2018-03-24 11:45:37 +0530

    client file modified

--- WishesClient.java
+++ WishesClient.java
@@ -9,5 +9,14 @@ class WishesClient
      DataInputStream din= new DataInputStream(s.getInputStream());
      DataOutputStream dout= new DataOutputStream(s.getOutputStream());
      BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+     System.out.println("Start Conversation");
+     System.out.println("Type 'stop' to end the conversation");
+    
+     String str="",str2="";
+     while(!str.equals("stop"))
+     {
+      str=br.readLine();
+     
   }
 }
