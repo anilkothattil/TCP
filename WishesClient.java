616a5aef44c32568fef601d03ddecef43bf0cd8f
anilagraceprakash <anilagracep@gmail.com>
2018-03-24 11:24:22 +0530

    client file modified

--- WishesClient.java
+++ WishesClient.java
@@ -1,11 +1,11 @@
 import java.net.*; 
 import java.io.OutputStream;
 
-public class WishesClient
+class WishesClient
 {
    public static void main(String args[]) throws Exception
    {
-     Socket sock = new Socket("localhost", 5000);
+     Socket sock = new Socket("localhost", 3333);
 	InetAddress ip=sock.getInetAddress();
 	System.out.println(ip);       
      String message1 = "Accept Best Wishes, Server";
